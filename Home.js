7efa7ccd6d7d7b07d9ce0a17452bcc9a764f4b39
turbotopup88-crypto <turbotopup88@gmail.com>
2026-05-28 07:46:46 +0300

    Update banner image URL in Home.js

--- Home.js
+++ Home.js
@@ -325,7 +325,7 @@ document.getElementById("bannerImage");
 
 const banners = [
 
-    "https://images.unsplash.com/photo-1542751371-adc38448a05e?w=1200",
+    "https://i.ibb.co/VW4QxFFX/20260528-074526.jpg",
 
     
     "https://images.unsplash.com/photo-1511512578047-dfb367046420?w=1200",
