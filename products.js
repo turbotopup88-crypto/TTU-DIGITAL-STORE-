109a7a574ce8d551ea0c01fdb4abf2d4b998659d
turbotopup88-crypto <turbotopup88@gmail.com>
2026-07-25 17:55:18 +0300

    Update product image URL in products.js

--- products.js
+++ products.js
@@ -411,7 +411,7 @@ const PRODUCTS = {
     inStock: true
 ,
     currency: { ar: "ج.م", en: "EGP" },
-    image: "https://i.ibb.co/QRMRrqn/2-20260702-162453.png",
+    image: "https://i.ibb.co/JjRLYQSQ/ey-Jp-ZCI6-Im1f-Nm-E2-NGNk-Y2-I5-OTQ0-ODE5-MWIw-MGMz-OTk5-OTgx-ZWFm-Ym-M6c2-Vka-W1lbn-Q6-Ly80-Nj-Q0-ZTNk-ZTk0-MD.jpg",
     description:
       "بطاقة هدايا 100 دولار امريكي لمنصة playstation، تسليم خلال ساعتين بحد اقصى بعد تأكيد الطلب عبر واتساب.",
     descriptionEn:
