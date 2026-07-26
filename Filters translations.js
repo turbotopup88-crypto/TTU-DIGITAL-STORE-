5ecba080f54401db9b819bf9bab803ad0edbfaf2
turbotopup88-crypto <turbotopup88@gmail.com>
2026-07-26 11:38:39 +0300

    Add translations for filters in Arabic and English
    Added Arabic and English translations for filters.

--- Filters translations.js
+++ Filters translations.js
@@ -1,1 +1,49 @@
+// ---------------------------------------------------------
+// إضافات الترجمات للفلاتر
+// أضف هذه الترجمات إلى ملف lang.js الخاص بك
+// ---------------------------------------------------------
 
+// أضف هذه الكائنات إلى الترجمات الموجودة:
+
+/*
+// في كائن translations.ar
+filters: "الفلاتر",
+clearFilters: "مسح الفلاتر",
+sortByPrice: "ترتيب حسب السعر",
+lowToHigh: "الأقل للأعلى",
+highToLow: "الأعلى للأقل",
+filterByDiscount: "تصفية",
+onlyDiscounted: "المنتجات بالخصم",
+
+// في كائن translations.en
+filters: "Filters",
+clearFilters: "Clear Filters",
+sortByPrice: "Sort by Price",
+lowToHigh: "Low to High",
+highToLow: "High to Low",
+filterByDiscount: "Filter",
+onlyDiscounted: "Discounted Products",
+*/
+
+// مثال كامل على كيفية إضافتها:
+
+const FILTERS_TRANSLATIONS = {
+  ar: {
+    filters: "الفلاتر",
+    clearFilters: "مسح الفلاتر",
+    sortByPrice: "ترتيب حسب السعر",
+    lowToHigh: "الأقل للأعلى",
+    highToLow: "الأعلى للأقل",
+    filterByDiscount: "تصفية",
+    onlyDiscounted: "المنتجات بالخصم"
+  },
+  en: {
+    filters: "Filters",
+    clearFilters: "Clear Filters",
+    sortByPrice: "Sort by Price",
+    lowToHigh: "Low to High",
+    highToLow: "High to Low",
+    filterByDiscount: "Filter",
+    onlyDiscounted: "Discounted Products"
+  }
+};
